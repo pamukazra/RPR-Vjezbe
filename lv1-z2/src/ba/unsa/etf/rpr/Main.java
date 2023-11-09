c7a934b3b33410b7d2fbd35a5c21517b521020ab
Azra Pamuk <apamuk1@etf.unsa.ba>
2023-11-09 14:13:33 +0100

    lv1-z2

---
+++ lv1-z2/src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+public class Main {
+    public static int sumaCifara(int n){
+        int suma=0;
+        while (n!=0){
+            suma+=n%10;
+            n/=10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Unesite broj n:");
+        int n=scanner.nextInt();
+        for (int i=1;i<=n;i++)
+            if (i%(sumaCifara(i))==0) System.out.println(i);
+    }
+}
