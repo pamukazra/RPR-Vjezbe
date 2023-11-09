5dc9f64e127c6c931d1a30c6ea8786fc2ab87ab5
Azra Pamuk <apamuk1@etf.unsa.ba>
2023-11-09 14:10:53 +0100

    lv1-z1

---
+++ lv1-z1/Zadatak1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Zadatak1{
+ public static void main(String[] args) {
+	Scanner n=new Scanner(System.in);
+	System.out.println("Unesite prvi broj:");
+	int a=n.nextInt();
+	System.out.println("Unesite drugi broj:");
+	int b=n.nextInt();
+	System.out.println("Prvi broj je "+a+", a drugi broj je "+b);
+ }
+}
